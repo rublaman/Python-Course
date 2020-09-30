d2e73eed73925616ab091f931f0f521e2467aadb
Ruben <rubensamg@gmail.com>
2020-09-30 21:54:18 +0200

    Uso de tuplas

---
+++ the_basics/tupletypes.py
@@ -0,0 +1,15 @@
+#---------------------------------------------------------------------
+# Una tupla es una coleccion ordenada e inmutable. Se escriben con ()
+#---------------------------------------------------------------------
+
+notastupla = (10, 3, 33, 54, 67, 58)
+print(notastupla)
+
+print(notastupla[1])
+
+print(notastupla[-1])
+
+print(notastupla[2:5])
+
+print(notastupla[-4:-1])
+
