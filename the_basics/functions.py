5c4188014f48b958067b22e25f1b2601aa8fd3ab
Ruben <rubensamg@gmail.com>
2020-10-08 22:59:35 +0200

    Funciones añadidas en functions.py

--- the_basics/functions.py
+++ the_basics/functions.py
@@ -13,3 +13,24 @@ def medianotas(milista):
     return milista
 
 print(medianotas(nota_estudiantes))
+
+# Llamada a una funcion con dos argumentos. Si llamamos a la
+# funcion con 1 o 3 argumentos, obtendremos un error.
+def mi_funcion2(nombre, apellido):
+    print("Hola " + nombre + " " + apellido)
+
+mi_funcion2("Ruben", "Blanco")
+
+# El parametro *args se usa para un numero desconocido de argumentos posicionales.
+# El parametro *args recibe los argumentos como una tupla
+def suma(*nums):
+    value = 0
+    for n in nums:
+        value += n
+    return value
+
+print(suma(2,3))
+
+# El parametro **kwargs se usa para pasar de forma opcional a una funcion un numero de
+# variables de argumentos con nombre. El parametro **kwargs recibe los argumentos como
+# diccionario
