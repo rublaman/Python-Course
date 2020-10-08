02038e745d4f22e2cbc14714750b96e4ac3868fd
Ruben <rubensamg@gmail.com>
2020-10-08 23:50:05 +0200

    Tuplas añadidas en tupletypes.py

--- the_basics/tupletypes.py
+++ the_basics/tupletypes.py
@@ -5,8 +5,10 @@
 notastupla = (10, 3, 33, 54, 67, 58)
 print(notastupla)
 
+# Acceso a tuplas
 print(notastupla[1])
 
+# Imprime usando index negativo
 print(notastupla[-1])
 
 print(notastupla[2:5])
