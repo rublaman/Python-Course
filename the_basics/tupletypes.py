9db18a768082f056e3903d2099a686b0a1181f7e
Ruben <rubensamg@gmail.com>
2020-10-20 18:09:10 +0200

    Comentarios añadidos en tupletypes.py

--- the_basics/tupletypes.py
+++ the_basics/tupletypes.py
@@ -1,8 +1,11 @@
 #---------------------------------------------------------------------
 # Una tupla es una coleccion ordenada e inmutable. Se escriben con ()
+# No podemos ni añadir ni eliminar elementos una vez creada la tupla,
+# pero si podemos unir varias tuplas
 #---------------------------------------------------------------------
 
 notastupla = (10, 3, 33, 54, 67, 58)
+notastupla2 = ("hola", "333", 75)
 print(notastupla)
 
 # Acceso a tuplas
@@ -11,6 +14,17 @@ print(notastupla[1])
 # Imprime usando index negativo
 print(notastupla[-1])
 
+
+# Imprime usando un rango
 print(notastupla[2:5])
 
+# Imprime usando rangos negativos
 print(notastupla[-4:-1])
+
+# Imprime todos los elemntos, uno por uno
+for n in notastupla:
+    print(n)
+
+# Unir dos tuplas
+notastupla3 = notastupla +  notastupla2
+print(notastupla3)
