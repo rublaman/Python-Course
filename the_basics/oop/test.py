7864b6efb912c3fb4bda511780dc290569ad49f0
Ruben <rubensamg@gmail.com>
2020-11-05 11:44:14 +0100

    Metodos añadidos en test.py

--- the_basics/oop/test.py
+++ the_basics/oop/test.py
@@ -2,6 +2,8 @@
 
 # clase: una coleccion de atributos que estan definidos para cualquier objeto
 
+import math
+
 class Complex:
     "esta clase simula numeros complejos"
     def __init__(self, real, imag):
@@ -9,12 +11,33 @@ class Complex:
         if(type(real) not in (int, float)) or type(imag) not in (int,float):
             raise Exception("Los argumentos no son numeros")
         
-        self.real = real
-        self.imag = imag
+        self.__real = real
+        self.__imag = imag
 
-try:
-    c = Complex(2, "a")
-    print(c.real, c.real)
-except Exception as e:
-    print(e)
+    def getReal(self):
+        return self.__real
+
+    def getImag(self):
+        return self.__imag
+
+    def getModulus(self):
+        return math.sqrt(self.getReal() ** 2 + self.getImag() ** 2)
+
+    def getPhi(self):
+        return math.atan2(self.getReal(), self.getImag())
+
+    def setReal(self, val):
+        if type(val) not in (int, float):
+            raise Exception("La parte real debe ser un numero")
+        self.__real = val
+
+    def setImag(self, val):
+        if type(val) not in (int, float):
+            raise Exception("La parte imag debe ser un numero")
+        self.__imag = val
+
+
+c = Complex(2.5, 3)
+
+print(c.getModulus(), c.getPhi())
 
