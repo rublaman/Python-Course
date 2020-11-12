9a6e0003f8b07e02390d0aa61e4c2c5093f0df62
Ruben <rubensamg@gmail.com>
2020-11-12 20:48:54 +0100

    Ejercicio 56 terminado

--- exercices/ej56.py
+++ exercices/ej56.py
@@ -1,3 +1,11 @@
 
-# Almacena el diccionario en un fichero JSON
+# Almacena el diccionario en un fichero JSON. 
 
+d = {"employees":
+        [{"firstName": "John", "lastName": "Doe"},
+        {"firstName": "Anna", "lastName": "Smith"},
+        {"firstName": "Peter", "lastName": "Jones"}],
+    "owners":
+        [{"firstName": "Jack", "lastName": "Petter"},
+        {"firstName": "Jessy", "lastName": "Petter"}]}
+        
