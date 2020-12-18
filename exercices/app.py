51a811a7e9ed999b8e2309fb06295342db1d33e4
Ruben <rubensamg@gmail.com>
2020-12-18 17:08:26 +0100

    Añadido Flask

--- exercices/app.py
+++ exercices/app.py
@@ -3,9 +3,11 @@
 # desde el navegador web. Cuando le demos al botón de guardar, este
 # se deberá de guardar en un fichero de texto. Se recomienda usar Flask
 
+# El fichero se llama app.py al usar el comando "python -m flask run"
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello World"
+def home():
+    return "Hello, Flask!"
